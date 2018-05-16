4a5ba4f39dbedfd6eb048498f1f3972eda3a4cf4
koushiro <koushiro.cqx@gmail.com>
2018-05-16 23:30:46 +0800

    modify lib structure, add iterative speed

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,12 +1,14 @@
 extern crate pbar;
 use pbar::ProgressBar;
+
 use std::thread;
 use std::time::Duration;
 
 fn main() {
     let count = 500;
     let mut pbar = ProgressBar::new(count);
-    pbar.refresh_interval(Duration::from_millis(1));
+    pbar.refresh_interval(Duration::from_millis(1))
+        .show_title(false);
     for _ in 0..count {
         pbar.increase();
         thread::sleep(Duration::from_millis(10));
--- src/bar.rs
+++ src/bar.rs
@@ -1,81 +1,115 @@
 use std::io::{self, Write, Stdout, stdout};
 use std::time::{Duration, Instant};
+use std::iter::repeat;
 use std::thread;
 
 use terminal_size::{Width, terminal_size};
 
+const NANOS_PER_SEC: u32 = 1_000_000_000;
+
+macro_rules! repeat {
+    ($s: expr, $n: expr) => {{
+        &repeat($s).take($n).collect::<String>()
+    }}
+}
+
 pub struct ProgressBar {
-    output: Stdout,
-    out: String,
-    prefix: String,
-    bar: String,
-    suffix: String,
-
-    start_time: Instant,
-    last_refresh_time: Instant,
-    is_finished: bool,
-
     total: u64,
     current: u64,
 
+    title_fmt: String,
+    speed_fmt: String,
+    time_fmt: String,
+    percent_fmt: String,
+    bar_fmt: String,
+
+    start_time: Instant,
+    current_time: Instant,
+    last_refresh_time: Instant,
+    is_finished: bool,
+
     title: String,
-    bar_fmt: BarFormat,
+    bar_symbol: BarSymbol,
     refresh_interval: Duration,
     width: usize,
+    is_show_title: bool,
+    is_show_speed: bool,
+    is_show_time: bool,
+    is_show_bar: bool,
+    is_show_percent: bool,
 
-    pub is_show_title: bool,
-    pub is_show_percent: bool,
-    pub is_show_bar: bool,
-    pub is_show_counter: bool,
-    pub is_show_time: bool,
-    pub is_show_speed: bool,
+    output: Stdout,
 }
 
 impl ProgressBar {
+
     pub fn new(total: u64) -> ProgressBar {
-        let w = terminal_width();
+        let w = terminal_width() - 1;
+        println!("terminal width = {}", w + 1);
         ProgressBar {
-            output: stdout(),
-            prefix: String::with_capacity(w),
-            suffix: String::with_capacity(w),
-            bar: String::with_capacity(w),
-            out: String::with_capacity(w),
-
-            start_time: Instant::now(),
-            last_refresh_time: Instant::now(),
-            is_finished: false,
-
             total,
             current: 0,
 
-            title: String::with_capacity(w >> 1),
-            bar_fmt: BarFormat::new(),
-            refresh_interval: Duration::from_millis(200),
-            width: w,
+            title_fmt: String::with_capacity(w >> 2),
+            speed_fmt: String::with_capacity(w >> 2),
+            time_fmt: String::with_capacity(w >> 2),
+            percent_fmt: String::with_capacity(w >> 2),
+            bar_fmt: String::with_capacity(w),
 
-            is_show_title: false,
-            is_show_percent: true,
-            is_show_bar: true,
-            is_show_counter: true,
-            is_show_time: true,
+            start_time: Instant::now(),
+            current_time: Instant::now(),
+            last_refresh_time: Instant::now(),
+            is_finished: false,
+
+            title: String::with_capacity(w >> 2),
+            bar_symbol: BarSymbol::new(),
+            refresh_interval: Duration::from_millis(100),
+            width: w,
+            is_show_title: true,
             is_show_speed: true,
+            is_show_time: true,
+            is_show_bar: true,
+            is_show_percent: true,
+
+            output: stdout(),
         }
     }
 
-    pub fn title<T: Into<String>>(&mut self, title: T) -> &mut ProgressBar {
-        self.title = title.into();
+    pub fn reset(&mut self, total: u64) -> &mut ProgressBar {
+        self.total = total;
+        self.current = 0;
+
+        self.start_time = Instant::now();
+        self.last_refresh_time = Instant::now();
+        self.is_finished = false;
+
+        self.title_fmt.clear();
+        self.bar_symbol = BarSymbol::new();
+        self.refresh_interval = Duration::from_millis(200);
+        self.width = terminal_width() - 1;
+        println!("terminal width = {}", self.width);
+        self.is_show_title = true;
+        self.is_show_time = true;
+        self.is_show_speed = false;
+        self.is_show_bar = true;
+        self.is_show_percent = true;
         self
     }
 
-    pub fn format_bar<T: Into<String>>(&mut self, fmt: T) -> &mut ProgressBar {
-        let fmt = fmt.into();
-        if fmt.len() == 5 {
-            let v: Vec<&str> = fmt.split("").collect();
-            self.bar_fmt.start_symbol = v[1].to_string();
-            self.bar_fmt.fill_symbol = v[2].to_string();
-            self.bar_fmt.current_symbol = v[3].to_string();
-            self.bar_fmt.empty_symbol = v[4].to_string();
-            self.bar_fmt.end_symbol = v[5].to_string();
+    pub fn title<T: Into<String>>(&mut self, t: T) -> &mut ProgressBar {
+        self.title = t.into();
+        self
+    }
+
+    pub fn symbol<T: Into<String>>(&mut self, s: T) -> &mut ProgressBar {
+        let symbol = s.into();
+        if symbol.len() == 5 {
+            let v: Vec<&str> = symbol.split("").collect();
+            self.bar_symbol.start = v[1].to_string();
+            self.bar_symbol.fill = v[2].to_string();
+            self.bar_symbol.current = v[3].to_string();
+            self.bar_symbol.empty = v[4].to_string();
+            self.bar_symbol.end = v[5].to_string();
         }
         self
     }
@@ -90,61 +124,113 @@ impl ProgressBar {
         self
     }
 
-    fn write(&mut self) -> io::Result<()> {
-        let title_fmt = format!("{} ", self.title);
-        let percent = self.current as f64 / self.total as f64 * 100f64;
-        let percent_fmt = format!("{:.*}% ", 0, percent);
-
-        if self.is_show_title {
-            self.prefix += &title_fmt;
-        }
-        if self.is_show_percent {
-            self.prefix += &percent_fmt;
-        }
-
-        let counter_fmt = format!("{}/{} ", self.current, self.total);
-        let time_fmt = format!("{} ", "time");
-        let speed_fmt = format!("{} ", "speed");
-        if self.is_show_counter {
-            self.suffix += &counter_fmt;
-        }
-        if self.is_show_time {
-            self.suffix += &time_fmt;
-        }
-        if self.is_show_speed {
-            self.suffix += &speed_fmt;
-        }
-
-        let bar_len = self.width - self.prefix.len() - self.suffix.len();
-        let bar_fmt = format!("{}{}{}{}{} ",
-                              self.bar_fmt.start_symbol,
-                              self.bar_fmt.fill_symbol,
-                              self.bar_fmt.current_symbol,
-                              self.bar_fmt.empty_symbol,
-                              self.bar_fmt.end_symbol);
-        if self.is_show_bar {
-            self.bar += &bar_fmt;
-        }
-
-        self.out = format!("\r{}{}{}", &self.prefix, &self.bar, &self.suffix);
-        self.output.write(self.out.as_bytes())?;
-        self.output.flush()?;
-
-        self.prefix.clear();
-        self.bar.clear();
-        self.suffix.clear();
-        self.out.clear();
-
-        Ok(())
+    pub fn show_title(&mut self, flag: bool) -> &mut ProgressBar {
+        self.is_show_title = flag;
+        self
     }
 
-    pub fn run(&mut self) {
-//        thread::spawn(|| {
-//            while !self.is_finished {
-//                self.update();
-//            }
-//            self.finish();
-//        });
+    pub fn show_percent(&mut self, flag: bool) -> &mut ProgressBar {
+        self.is_show_percent = flag;
+        self
+    }
+
+    pub fn show_bar(&mut self, flag: bool) -> &mut ProgressBar {
+        self.is_show_bar = flag;
+        self
+    }
+
+    pub fn show_time(&mut self, flag: bool) -> &mut ProgressBar {
+        self.is_show_time = flag;
+        self
+    }
+
+    pub fn show_speed(&mut self, flag: bool) -> &mut ProgressBar {
+        self.is_show_speed = flag;
+        self
+    }
+
+    fn fmt_title(&mut self) -> &mut ProgressBar {
+        if self.is_show_title {
+            let len = self.width >> 2;
+            self.title_fmt = format!("{:<width$} ", self.title, width = len);
+        }
+        self
+    }
+
+    fn fmt_speed(&mut self) -> &mut ProgressBar {
+        if self.is_show_speed {
+            let elapsed_time
+                = self.current_time.duration_since(self.start_time);
+            let speed = self.current as f64 /
+                (elapsed_time.as_secs() as f64 + elapsed_time.subsec_nanos() as f64 / NANOS_PER_SEC as f64);
+            self.speed_fmt = format!("{:.*}it/s ", 2, speed);
+        }
+        self
+    }
+
+    fn fmt_time(&mut self) -> &mut ProgressBar {
+        if self.is_show_time {
+
+            self.time_fmt = format!("{} ", "time");
+        }
+        self
+    }
+
+    fn fmt_bar(&mut self) -> &mut ProgressBar {
+        if self.is_show_bar {
+            let percent = self.current as f64 / self.total as f64;
+            let percent_len = 4usize;
+
+            let bar_len = self.width - self.title_fmt.len()
+                - self.speed_fmt.len() - self.time_fmt.len() - percent_len - 3;
+            let fill_len = (percent * bar_len as f64) as usize;
+            let empty_len = bar_len - fill_len;
+
+            let bar_fmt = match self.is_finished {
+                false => {
+                    format!("{}{}{}{}{} ",
+                            self.bar_symbol.start,
+                            repeat!(self.bar_symbol.fill.to_owned(), fill_len),
+                            self.bar_symbol.current,
+                            repeat!(self.bar_symbol.empty.to_owned(), empty_len),
+                            self.bar_symbol.end)
+                },
+                true => {
+                    format!("{}{}{} ",
+                            self.bar_symbol.start,
+                            repeat!(self.bar_symbol.fill.to_owned(), fill_len),
+                            self.bar_symbol.end)
+                },
+            };
+            self.bar_fmt = bar_fmt;
+        }
+        self
+    }
+
+    fn fmt_percent(&mut self) -> &mut ProgressBar {
+        if self.is_show_percent {
+            let percent = (self.current as f64 / self.total as f64 * 100f64) as u8;
+            self.percent_fmt = format!("{}%", percent);
+        }
+        self
+    }
+
+    fn write(&mut self) -> io::Result<()> {
+        self.fmt_title().fmt_speed().fmt_time().fmt_bar().fmt_percent();
+
+        self.output.write_fmt(format_args!(
+            "\r{}{}{}{}{}",
+            &self.title_fmt, &self.speed_fmt, &self.time_fmt,
+            &self.bar_fmt, &self.percent_fmt))?;
+        self.output.flush()?;
+
+        self.title_fmt.clear();
+        self.speed_fmt.clear();
+        self.time_fmt.clear();
+        self.bar_fmt.clear();
+        self.percent_fmt.clear();
+
+        Ok(())
     }
 
     fn finish(&mut self) {
@@ -162,6 +248,8 @@ impl ProgressBar {
     }
 
     pub fn set(&mut self, value: u64) -> u64 {
+        self.current_time = Instant::now();
+
         if value < self.total {
             self.current = value;
         } else {
@@ -191,28 +279,35 @@ impl ProgressBar {
 fn terminal_width() -> usize {
     let size = terminal_size();
     if let Some((Width(w), _)) = size {
+        println!("Some terminal width");
         w as usize
     } else {
+        println!("None terminal width");
         80
     }
 }
 
-pub struct BarFormat {
-    start_symbol: String,
-    fill_symbol: String,
-    current_symbol: String,
-    empty_symbol: String,
-    end_symbol: String,
+struct BarSymbol {
+    start: String,
+    fill: String,
+    current: String,
+    empty: String,
+    end: String,
 }
 
-impl BarFormat {
-    pub fn new() -> BarFormat {
-        BarFormat {
-            start_symbol: String::from("["),
-            fill_symbol: String::from("#"),
-            current_symbol: String::from(">"),
-            empty_symbol: String::from("-"),
-            end_symbol: String::from("]"),
+impl BarSymbol {
+    pub fn new() -> BarSymbol {
+        BarSymbol {
+            start: String::from("["),
+            fill: String::from("#"),
+            current: String::from(">"),
+            empty: String::from(" "),
+            end: String::from("]"),
         }
     }
+}
+
+#[cfg(test)]
+mod test {
+
 }
